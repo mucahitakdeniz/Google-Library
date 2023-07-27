59dd49ecb6a5c0fbcd759692172738de5ede1f8f
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-27 09:35:33 +0300

    yarn

---
+++ google-library/src/components/card/Card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const url =
+  "`https://www.googleapis.com/books/v1/volumes?q=${input}&printType=${type}&key=AIzaSyCvTpnIzXtadNKxNDhwpspdEa8l1rcidt4";
+const Card = () => {
+  return <div>Card</div>;
+};
+
+export default Card;
