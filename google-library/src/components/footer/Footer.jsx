10e17721b6725e6b93073f9f42cd3c619b539c57
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-28 15:31:02 +0300

    footer

--- google-library/src/components/footer/Footer.jsx
+++ google-library/src/components/footer/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { lightIcon, import { useThemeContext } from "../../context/ThemeContext";
+import { lightIcon, darkIcon } from "../../helper/iconData";
+import { FooterBtn, FooterContainer } from "./Footer.style";
 
 const Footer = () => {
+  const { myTheme, setMyTheme } = useThemeContext();
   return (
-    <div>Footer</div>
-  )
-}
+    <FooterContainer>
+      <FooterBtn onClick={handleClick}>
+        {myTheme === "light" ? lightIcon : darkIcon}
+      </FooterBtn>
+    </FooterContainer>
+  );
+};
 
-export default Footer
+export default Footer;
